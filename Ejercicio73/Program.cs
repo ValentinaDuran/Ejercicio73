d480b9b086373de838640a8ec8a4369dc6b94190
duran.valentina@hotmail.com <duran.valentina@hotmail.com>
2022-09-30 18:16:03 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio73/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio73
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadena;
+
+            Queue<string> colaStr = new Queue<string>();
+
+            Console.WriteLine("ingrese una cadena de texto");
+            cadena = Console.ReadLine();
+
+            while (cadena != "")
+            {
+                colaStr.Enqueue(cadena);
+                Console.WriteLine("ingrese una cadena de texto");
+                cadena = Console.ReadLine();
+
+            }
+            while (colaStr.Count > 0)
+            {
+                Console.WriteLine(colaStr.Dequeue());
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
